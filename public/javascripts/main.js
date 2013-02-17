e22d6ebe5b8f00a50ab6aef3dfd7ca66fe4dd4d3
Ryan Jadhav <ryan.jadhav@gmail.com>
2013-02-16 16:25:08 -0800

    rdio stuff

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,14 @@
-var LYRICFIND_DISPLAY_KEY = 'asdad';
-var LYRICFIND_LRC_KEY = 'asdas';
-var LYRICFIND_SEARCH_KEY = 'asdas';
+var LYRICFIND_DISPLAY_KEY = 'asdad',
+    LYRICFIND_LRC_KEY = 'asdas',
+    LYRICFIND_SEARCH_KEY = 'asdas',
+    Rdio = {};
 
 ;(function($) {
 
-  // Code goes here.
+  R.ready(function() {
+    Rdio = new RdioHelper();
+  });
+
 })(window.jQuery);
 
 function search(query) {
@@ -17,8 +21,8 @@ function search(query) {
   };
 
   var searchUrl = 'http://api.lyricfind.com/search.do?' +  $.param(searchParams);
-  
+
   return searchUrl;
-  
+
   // ajax search url and get trackid
 }
