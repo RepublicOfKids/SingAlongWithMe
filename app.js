f161b7cbc1e00dd41044f237fc73ff2577e6aa6c
Ryan Jadhav <ryan.jadhav@gmail.com>
2013-02-17 12:49:35 -0800

    storing it on a port?

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * Module dependencies.
  */
 
-process.env.PORT = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 var express = require('express'),
     routes  = require('./routes'),
@@ -10,7 +10,7 @@ var express = require('express'),
     http    = require('http'),
     path    = require('path'),
     app     = express(),
-    server  = app.listen(process.env.PORT), //require('http').createServer(app),
+    server  = app.listen(port), //require('http').createServer(app),
     io      = require('socket.io').listen(server);
 
 app.configure(function(){
@@ -72,5 +72,5 @@ io.sockets.on('connection', function (socket) {
 
 http.createServer(app).listen(app.get('port'), function(){
   //  console.log("Express server listening on port " + app.get('port'));
-  console.log("Express server listening on port " + process.env.PORT);
+  console.log("Express server listening on port " + port;
 });
